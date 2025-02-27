bcea41ab0ca83033f62ca5938b4204946a3d05f8
iosman456 <muhammetcanaltan41@gmail.com>
2025-02-27 17:30:49 +0300

    isetup.py

--- isetup.py
+++ isetup.py
@@ -2,3 +2,9 @@ import os
 
 print("Python2 Kuruluyor...")
 os.system("apk add python2")
+print("Python2 Kuruldu!")
+
+print("Python3 Kuruluyor...")
+os.system("apk add python3")
+print("Python3 Kuruldu!")
+
