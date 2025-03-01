6f4856106cf1f9dac7573a0e07129ed9f02187b5
iosman456 <muhammetcanaltan41@gmail.com>
2025-03-01 13:01:30 +0300

    Update isetup.py

--- isetup.py
+++ isetup.py
@@ -1,42 +1,9 @@
-#!/usr/bin/env python3
-
 import os
 
-def update_system():
-    print("İOS Update Oluyor...")
-    os.system("apk update")
-    print("Oldu!")
-
-def upgrade_system():
-    print("İOS Upgrade Oluyor...")
-    os.system("apk upgrade")
-    print("Oldu!")
-
-def install_python2():
-    print("Python2 Kuruluyor...")
-    os.system("apk add python2")
-    print("Python2 Kuruldu!")
-
-def install_python3():
-    print("Python3 Kuruluyor...")
-    os.system("apk add python3")
-    print("Python3 Kuruldu!")
-
-def install_php():
-    print("PHP Kuruluyor...")
-    os.system("apk add php")
-    print("PHP Kuruldu!")
-
-def install_neofetch():
-    print("neofetch Hazılanıyor...")
-    os.system("apk add neofetch")
-    print("neofetch Hazırlandı! Hatta Görünümüde Böyle.")
-    os.system("neofetch")
-
-def install_tmux_htop():
-    print("TMUX HTOP Kuruluyor...")
-    os.system("apk add tmux htop")
-    print("Tüm Paketler bitti, Görüsürüz")
+def show_disk_usage():
+    print("Disk Kullanımı Gösteriliyor...")
+    os.system("df -h")
+    print("Disk Kullanımı Gösterildi!")
 
 def main_menu():
     while True:
@@ -48,7 +15,8 @@ def main_menu():
         print("5. PHP Kur")
         print("6. neofetch Kur")
         print("7. TMUX ve HTOP Kur")
-        print("8. Çıkış")
+        print("8. Disk Kullanımını Göster")
+        print("9. Çıkış")
 
         choice = input("Bir seçenek giriniz: ")
 
@@ -67,10 +35,9 @@ def main_menu():
         elif choice == '7':
             install_tmux_htop()
         elif choice == '8':
+            show_disk_usage()
+        elif choice == '9':
             print("Çıkış yapılıyor...")
             break
         else:
             print("Geçersiz seçenek, lütfen tekrar deneyin.")
-
-if __name__ == "__main__":
-    main_menu()
