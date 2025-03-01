df9ea9a94d797ca940d7060e9197ebdbfb09684a
iosman456 <muhammetcanaltan41@gmail.com>
2025-03-01 04:27:41 +0300

    Update isetup.py

--- isetup.py
+++ isetup.py
@@ -1,29 +1,13 @@
 import os
-import subprocess
-import pyfiglet
 
-def figlet_yaz(text):
-    ascii_art = pyfiglet.figlet_format(text)
-    print(ascii_art)
+print("Python2 Kuruluyor...")
+os.system("apk add python2")
+print("Python2 Kuruldu!")
 
-def tool():
-    figlet_yaz("iSetup")
+print("Python3 Kuruluyor...")
+os.system("apk add python3")
+print("Python3 Kuruldu!")
 
-    try:
-        print("Python2 Kuruluyor...")
-        os.system("apk add python2")
-        print("Python2 Kuruldu!")
-
-        print("Python3 Kuruluyor...")
-        os.system("apk add python3")
-        print("Python3 Kuruldu!")
-
-        print("PHP Kuruluyor...")
-        os.system("apk add php")
-        print("PHP Kuruldu!")
-    except Exception as e:
-        print(f"Hata oluştu: {e}")
-        subprocess.run(["python3", "fix.py"])
-
-if __name__ == "__main__":
-    tool()
+print("PHP Kuruluyor...")
+os.system("apk add php")
+print("PHP Kuruldu!")
