79e69fb6f240abfcba5c11ab3335b2211aa95bf7
iosman456 <muhammetcanaltan41@gmail.com>
2025-03-01 13:17:06 +0300

    Update isetup.py

--- isetup.py
+++ isetup.py
@@ -2,54 +2,71 @@
 
 import os
 
+# ANSI color codes for rainbow effect
+colors = [
+    "\033[91m",  # Red
+    "\033[93m",  # Yellow
+    "\033[92m",  # Green
+    "\033[96m",  # Cyan
+    "\033[94m",  # Blue
+    "\033[95m",  # Purple
+    "\033[0m"    # Reset
+]
+
+def color_print(text):
+    for i, char in enumerate(text):
+        color = colors[i % len(colors)]
+        print(f"{color}{char}", end="")
+    print(colors[-1])  # Reset at the end
+
 def update_system():
-    print("İOS Update Oluyor...")
+    color_print("İOS Update Oluyor...")
     os.system("apk update")
-    print("Oldu!")
+    color_print("Oldu!")
 
 def upgrade_system():
-    print("İOS Upgrade Oluyor...")
+    color_print("İOS Upgrade Oluyor...")
     os.system("apk upgrade")
-    print("Oldu!")
+    color_print("Oldu!")
 
 def install_python2():
-    print("Python2 Kuruluyor...")
+    color_print("Python2 Kuruluyor...")
     os.system("apk add python2")
-    print("Python2 Kuruldu!")
+    color_print("Python2 Kuruldu!")
 
 def install_python3():
-    print("Python3 Kuruluyor...")
+    color_print("Python3 Kuruluyor...")
     os.system("apk add python3")
-    print("Python3 Kuruldu!")
+    color_print("Python3 Kuruldu!")
 
 def install_php():
-    print("PHP Kuruluyor...")
+    color_print("PHP Kuruluyor...")
     os.system("apk add php")
-    print("PHP Kuruldu!")
+    color_print("PHP Kuruldu!")
 
 def install_neofetch():
-    print("neofetch Kuruluyor...")
+    color_print("neofetch Kuruluyor...")
     os.system("apk add neofetch")
-    print("neofetch Kuruldu!")
+    color_print("neofetch Kuruldu!")
     os.system("neofetch")
 
 def install_tmux_htop():
-    print("TMUX ve HTOP Kuruluyor...")
+    color_print("TMUX ve HTOP Kuruluyor...")
     os.system("apk add tmux htop")
-    print("TMUX ve HTOP Kuruldu!")
+    color_print("TMUX ve HTOP Kuruldu!")
 
 def install_nano():
-    print("nano Kuruluyor...")
+    color_print("nano Kuruluyor...")
     os.system("apk add nano")
-    print("nano Kuruldu!")
+    color_print("nano Kuruldu!")
 
 def install_vim():
-    print("Vim Kuruluyor...")
+    color_print("Vim Kuruluyor...")
     os.system("apk add vim")
-    print("Vim Kuruldu!")
+    color_print("Vim Kuruldu!")
 
 def install_all():
-    print("Bütün Paketler Toplanıyor...")
+    color_print("Bütün Paketler Toplanıyor...")
     update_system()
     upgrade_system()
     install_python2()
@@ -59,22 +76,22 @@ def install_all():
     install_tmux_htop()
     install_nano()
     install_vim()
-    print("Tüm Paketler Kuruldu, Görüşürüz!")
+    color_print("Tüm Paketler Kuruldu, Görüşürüz!")
 
 def main_menu():
     while True:
-        print("\nAna Menü")
-        print("1. Sistem Güncelle")
-        print("2. Sistem Yükselt")
-        print("3. Python2 Kur")
-        print("4. Python3 Kur")
-        print("5. PHP Kur")
-        print("6. neofetch Kur")
-        print("7. TMUX ve HTOP Kur")
-        print("8. nano Kur")
-        print("9. Vim Kur")
-        print("10. Hepsini Aynı Anda Kur")
-        print("11. Çıkış")
+        color_print("\nAna Menü")
+        color_print("1. Sistem Güncelle")
+        color_print("2. Sistem Yükselt")
+        color_print("3. Python2 Kur")
+        color_print("4. Python3 Kur")
+        color_print("5. PHP Kur")
+        color_print("6. neofetch Kur")
+        color_print("7. TMUX ve HTOP Kur")
+        color_print("8. nano Kur")
+        color_print("9. Vim Kur")
+        color_print("10. Hepsini Aynı Anda Kur")
+        color_print("11. Çıkış")
 
         choice = input("Bir seçenek giriniz: ")
 
@@ -99,10 +116,10 @@ def main_menu():
         elif choice == '10':
             install_all()
         elif choice == '11':
-            print("Çıkış yapılıyor...")
+            color_print("Çıkış yapılıyor...")
             break
         else:
-            print("Geçersiz seçenek, lütfen tekrar deneyin.")
+            color_print("Geçersiz seçenek, lütfen tekrar deneyin.")
 
 if __name__ == "__main__":
     main_menu()
